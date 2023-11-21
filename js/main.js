a73c91e13ca2c36102e0a7400ce3538531a0e44e
eirwin <eirwin@umich.edu>
2023-11-21 00:00:26 -0500

    really cool and fun lack of commit this whole time

---
+++ js/main.js
@@ -0,0 +1,8 @@
+  window.addEventListener('scroll', function() {
+    const nav = document.querySelector("nav"); 
+    // if (window.scrollY > 100) {
+      nav.style.top = "0px";
+    // } else {
+    //   nav.style.top = "-100px";
+    // }
+  });
